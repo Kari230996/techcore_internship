044467bd40a19151e32129dc71eb52e91c62dbad
Karina <claro.claire1996@gmail.com>
2025-10-22 22:11:14 +0300

    Fix TASK-271: Базовый синтаксис и типы

--- calculator.py
+++ calculator.py
@@ -6,24 +6,44 @@ b = input("Введите второе число: ")
 operation = input("Выберите операцию (+, -, *, /): ")
 
 try:
-    a = float(a)
-    b = float(b)
+    a_val = float(a)
+    b_val = float(b)
+    is_number = True
 
 except ValueError:
-    print("Ошибка: введите число!")
-    exit()
+    is_number = False
 
-if operation == "+":
-    result = a + b
-elif operation == "-":
-    result = a - b
-elif operation == "*":
-    result = a * b
-elif operation == "/":
-    if b != 0:
-        result = a / b
+# Работа с числами
+if is_number:
+    if operation == "+":
+        result = a_val + b_val
+    elif operation == "-":
+        result = a_val - b_val
+    elif operation == "*":
+        result = a_val * b_val
+    elif operation == "/":
+        if b_val != 0:
+            result = a_val / b_val
+        else:
+            print("Ошибка: деление на ноль!")
+            exit()
     else:
-        print("Ошибка: деление на ноль!")
+        print("Ошибка: неизвестная операция.")
+        exit()
+
+# Работа со строками
+else:
+    if operation == "+":
+        result = a + b
+    elif operation == "*":
+        try:
+            b_int = int(b)
+            result = a * b_int
+        except ValueError:
+            print("Ошибка: при работе со строками можно только складывать или умножать строку на целое число.")
+            exit()
+    else:
+        print("Ошибка: при работе со строками доступны только '+' и '*' (на число).")
         exit()
     
 print("Результат:", result)
