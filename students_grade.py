b4f3ec0c2d1d53e6b0ac59f291cbe7d5165e1c79
Karina <claro.claire1996@gmail.com>
2025-10-22 13:06:09 +0300

    Fix TASK-271: Структуры данных (List и Dict)

--- students_grade.py
+++ students_grade.py
@@ -1,17 +1,20 @@
 students = [
-    {"name": "Гермиона", "age": 17, "grade": 5.0},
-    {"name": "Гарри", "age": 17, "grade": 4.2},
-    {"name": "Рон", "age": 16, "grade": 3.9},
-    {"name": "Люси", "age": 17, "grade": 4.8},
-    {"name": "Грегори", "age": 16, "grade": 4.5}
+    {"name": "Гермиона", "age": 17, "avg_grade": 5.0},
+    {"name": "Гарри", "age": 17, "avg_grade": 4.2},
+    {"name": "Рон", "age": 16, "avg_grade": 3.9},
+    {"name": "Люси", "age": 17, "avg_grade": 4.8},
+    {"name": "Грегори", "age": 16, "avg_grade": 4.5}
 ]
 
 def filter_students_by_grade(students_list, min_grade=4.0):
-    filtered = [student for student in students_list if student["grade"] >= min_grade]
+    filtered = []
+    for student in students_list:
+        if student["avg_grade"] > min_grade:
+            filtered.append(student)
     return filtered
 
 filtered_students = filter_students_by_grade(students, 4.0)
 
 print("Студенты со средним баллом выше 4.0:")
 for student in filtered_students:
-    print(f"Имя: {student['name']}, Средний балл: {student['grade']}")
+    print(f"Имя: {student['name']}, Средний балл: {student['avg_grade']}")
