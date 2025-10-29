364e28bdc3386b8256928dff10d560abfbc512cd
Karina <claro.claire1996@gmail.com>
2025-10-29 18:37:23 +0300

    TASK-279: AsyncEngine и AsyncSession

---
+++ Module 5/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from fastapi import Depends
+
+from models import Base
+
+
+# Настройка AsyncEngine
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/books_db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+
+async_session_maker = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession)
+
+
+# Dependency для FastApi 
+async def get_db() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
