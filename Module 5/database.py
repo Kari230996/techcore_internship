caccfe26a9cd40a45bfb4147ae66910c648700f9
Karina <claro.claire1996@gmail.com>
2025-10-30 11:47:55 +0300

    TASK-279: Асинхронные Транзакции

--- Module 5/database.py
+++ Module 5/database.py
@@ -8,14 +8,14 @@ from models import Base
 # Настройка AsyncEngine
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/books_db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, pool_pre_ping=True, )
 
 
 async_session_maker = async_sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession)
 
 
-# Dependency для FastApi 
+# Dependency для FastApi
 async def get_db() -> AsyncSession:
     async with async_session_maker() as session:
         yield session
--- Module 5/main.py
+++ Module 5/main.py
@@ -1,5 +1,4 @@
-import time
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
 from routers import books
 
