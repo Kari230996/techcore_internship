ebdfc2da08ad95e6556e7590ed36c2eccc07bd9e
Karina <claro.claire1996@gmail.com>
2025-10-31 18:22:50 +0300

    TASK-281: 'Полиглотный' Эндпоинт (Async Gather)

--- Module 6/main.py
+++ Module 6/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import books, reviews
+from app.routers import books, reviews, products
 
 
 app = FastAPI(title="TechCore Internship", version="1.0")
 
 app.include_router(books.router)
 app.include_router(reviews.router)
+app.include_router(products.router)
