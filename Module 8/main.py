e1cab071b506911866b82b32aa7cfaeaef714a2b
Karina <claro.claire1996@gmail.com>
2025-11-06 12:17:22 +0300

    TASK-285: Fallback (Запасной ответ)

--- Module 8/main.py
+++ Module 8/main.py
@@ -4,8 +4,13 @@ from author_service import AuthorService
 
 async def main():
     service = AuthorService("https://httpbin.org/status")
-    result = await service.get_author(503)
-    print("Результат:", result)
+
+    for i in range(6):
+        print(f"\n--- Запрос {i + 1} ---")
+
+        result = await service.get_author(503)
+        print(result)
+        await asyncio.sleep(0.5)
     await service.close()
 
 
