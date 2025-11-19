a786f5ba91ad621e2c7015a308bec89897f05e2d
Karina <claro.claire1996@gmail.com>
2025-11-19 15:24:02 +0300

    TASK-291: Gateway (Маршрутизация)

--- Module 11/gateway/main.py
+++ Module 11/gateway/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import httpx
 
 app = FastAPI(title="Gateway API")
+
+BOOK_SERVICE_URL = "http://book-service:8000"
 
 
 @app.get("/")
@@ -8,3 +11,16 @@ def root():
     return {"message": "Gatewway is running"}
 
 
+@app.get("/api/books/{book_id}")
+async def get_book(book_id: int):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{BOOK_SERVICE_URL}/api/books/{book_id}")
+        except httpx.RequestError:
+            raise HTTPException(
+                status_code=502, detail="Сервис недоступен")
+
+    if response.status_code == 404:
+        raise HTTPException(status_code=404, detail="Книга не найдена")
+
+    return response.json()
